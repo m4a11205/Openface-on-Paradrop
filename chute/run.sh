77aa5dc3996ca15e75c08ca5b9d916b3d1df012e
m4a11205 <m4a11205@gamil.com>
2017-03-13 15:20:17 -0500

    x

--- chute/run.sh
+++ chute/run.sh
@@ -9,9 +9,7 @@ chmod a+rw /var/www/html/motionLog
 
 # Execute the file, one pic every 2 seconds
 #python /usr/local/bin/smarthouse.py -m_sec 5.0 > smarthouse.log 2> smarthouse.err &
-#python /usr/local/bin/smarthouse.py -m_sec 5.0 infer /usr/local/bin/classifier.pkl test.jpg > smarthouse.log 2> smarthouse.err &
-
-#python face_classifier.py infer classifier.pkl /root/openface/test-images/curry/
+python /usr/local/bin/smarthouse.py -m_sec 5.0 infer /usr/local/bin/classifier.pkl test.jpg > smarthouse.log 2> smarthouse.err &
 
 # Add the symlink
 ln -s --relative /var/www/html/motionLog /var/www/html/app-dist/
