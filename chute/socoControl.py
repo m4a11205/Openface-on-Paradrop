7fb579f0ec0983b84a423e52c1031e8f75856ea9
squallee <lishuo78706@gmail.com>
2017-07-18 16:27:19 -0500

    fix continue play

--- chute/socoControl.py
+++ chute/socoControl.py
@@ -16,11 +16,13 @@ URL_BASE = {'ted': 'http://ia801402.us.archive.org/20/items/TenD2005-07-16.flac1
 }
 
 MUSIC_BASE = {'http://ia801402.us.archive.org/20/items/TenD2005-07-16.flac16/TenD2005-07-16t10Wonderboy.mp3':'Wonderboy',
- 'http://fmn.rrimg.com/fmn059/audio/20140822/0210/m_mTCE_490d00001683125d.mp3': 'm_mTCE_490d00001683125d.mp3'
+ 'http://fmn.rrimg.com/fmn059/audio/20140822/0210/m_mTCE_490d00001683125d.mp3': 'm_mTCE_490d00001683125d.mp3',
+ 'http://soundbible.com/mp3/School_Fire_Alarm-Cullen_Card-202875844.mp3': 'School_Fire_Alarm-Cullen_Card-202875844.mp3'
 }
-# Alarm sound #
+# sound #
 ALARM_URL = 'http://soundbible.com/mp3/School_Fire_Alarm-Cullen_Card-202875844.mp3'
-# Classic Music #
+START_URL = 'http://soundbible.com/mp3/dixie-horn_daniel-simion.mp3'
+END_URL = 'http://soundbible.com/mp3/Page_Turn-Mark_DiAngelo-1304638748.mp3'
 U0 = 'http://fmn.rrimg.com/fmn059/audio/20140822/0210/m_mTCE_490d00001683125d.mp3'
 U1 = 'http://ia801402.us.archive.org/20/items/TenD2005-07-16.flac16/TenD2005-07-16t10Wonderboy.mp3'
 
@@ -50,6 +52,7 @@ def run_SONO_App(sonos):
     print("\n SONO Speaker Controller App is ready !!!\n")
     app = create_SONO_App(sonos)
     app.run(host = '0.0.0.0', port = 8015)
+    
 
 
 class SonoController():
@@ -62,17 +65,31 @@ class SonoController():
     def play_by_userName(self, name):
         url = URL_BASE.get(name,"Unknown")
         if (url == "Unknown"):
-            self.core.play_uri(ALARM_URL)
+            track = self.core.get_current_track_info()
+            current_play = track['title']
+            # print track['title']
+            target_play = MUSIC_BASE.get(url, "Unknow")
+            # print target_play
+            if (current_play == target_play):
+                print "unknown person"
+            else:
+                self.core.clear_queue()
+                self.core.add_uri_to_queue(ALARM_URL)
+                self.core.add_uri_to_queue(END_URL)
+                self.cores.play_from_queue(0, True)
         else:
             track = self.core.get_current_track_info()
             current_play = track['title']
-            print track['title']
+            # print track['title']
             target_play = MUSIC_BASE.get(url, "Unknow")
-            print target_play
+            # print target_play
             if (current_play == target_play):
                 print "the same person"
             else:
-                self.core.play_uri(url)
+                self.core.clear_queue()
+                self.core.add_uri_to_queue(url)
+                self.core.add_uri_to_queue(END_URL)
+                self.cores.play_from_queue(0, True)
 
     def pause(self):
         self.core.pause()
@@ -95,7 +112,7 @@ class SonoController():
 def connectSpeaker():
     sono_ip = "192.168.128.181"
     sonos = SonoController(sono_ip)
-
+    sonos.play_uri(START_URL)
     ## start multi-thread to listening the flask packet
     try:
        thread.start_new_thread( run_SONO_App, (sonos,) )
@@ -118,10 +135,6 @@ if __name__ == '__main__':
 
     #sonos = SoCo(sono_ip) # Pass in the IP of your Sonos speaker
 
-    track = sonos.get_current_track_info()
-
-    print track['title']
-    print sonos.player_name()
     #sonos.pause()
 
     # Play a stopped or paused track
